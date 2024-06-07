465da890896e8dbb06feeb2f31449867b87cad52
Saurav Tiwari <116860218+saurav-tiwari03@users.noreply.github.com>
2024-06-07 14:41:27 +0530

    update

--- server/config/database.js
+++ server/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect = () => {
-    mongoose.connect("mongodb+srv://sanskarsingh9999:8Rv0vqxo7MMY3w4Q@cluster0.9xyxlf9.mongodb.net/StudyNotion")
+    mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log("DB Connected Successfully"))
     .catch( (error) => {
         console.log("DB Connection Failed");
--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -3,6 +3,6 @@ require('dotenv').config();
 
 
 exports.instance = new Razorpay({
-    key_id: process.env.RAZORPAY_KEY || "rzp_test_hlYUA90sOzvHSB",
+    key_id: process.env.RAZORPAY_KEY,
     key_secret: process.env.RAZORPAY_SECRET,
 });
