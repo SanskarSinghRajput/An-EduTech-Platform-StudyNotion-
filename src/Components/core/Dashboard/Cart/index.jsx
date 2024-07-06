8e42587359dad1cb95e8fc4bbb71ed08156b12b1
SanskarSinghRajput <you@example.com>
2024-07-06 19:16:02 +0530

    "Initial commit"

--- src/Components/core/Dashboard/Cart/index.jsx
+++ src/Components/core/Dashboard/Cart/index.jsx
@@ -14,11 +14,11 @@ export default function Cart() {
             <h1 className="mb-14 text-3xl font-medium text-richblack-5 montserrat">Cart</h1>
             <p className="border-b  border-b-richblack-400 pb-2 font-semibold text-richblack-400 crimson">{totalItems} Courses in Cart</p>
 
-            {total > 0 
+            {(total > 0) 
             ? (<div className="mt-1 flex flex-col-reverse items-start gap-x-10 gap-y-6 lg:flex-row">
                 <RenderCartCourses />
                 <RenderTotalAmount />
-            </div>)
+               </div>)
             : (<p>Your Cart is Empty</p>)}
         </div>
     )
